7d8db2185caeb1cb59f265d87c67f5a9e222a4df
Jeff Wong <awole20@gmail.com>
2016-05-11 17:44:04 -0700

    adding admin page, usertypes

--- src/clj/hey_friend/server.clj
+++ src/clj/hey_friend/server.clj
@@ -13,6 +13,20 @@
             [qarth.impl.google])
   (:gen-class))
 
+(derive ::admin ::user)
+
+(def admins #{"awole20@gmail.com"})
+
+(defn credential-fn [id]
+  (let [email (get-in id [:qarth.oauth/record :email])]
+    (if (contains? admins email)
+      (do
+        (println "admin " email)
+        (assoc id :roles [::admin]))
+      (do
+        (println "normal user " email)
+        (assoc id :roles [::user])))))
+
 (def conf {:type :google
            :callback (or (env :callback) "http://localhost:3449/login")
            :api-key (or (env :api-key) "")
@@ -20,10 +34,11 @@
 (def service (oauth/build conf))
 
 (def workflow
-  (qarth.friend/oauth-workflow {:service service
-                                :login-failure-handler
-                                (fn [_] (ring.util.response/redirect
-                                         "/login?exception=true"))}))
+  (qarth.friend/oauth-workflow
+   {:service service
+    :login-failure-handler
+    (fn [_] (ring.util.response/redirect
+             "/login?exception=true"))}))
 
 (defroutes routes
   (GET "/" request "open.")
@@ -40,13 +55,15 @@
                 "<br/> and friend attributes:"
                 friend-attributes
                 "</body></html>"))))
+  (GET "/admin" request
+       (friend/authorize #{::admin} "Only admins can see this page."))
   (friend/logout (ANY "/logout" request (ring.util.response/redirect "/"))))
 
 (def http-handler
   (-> routes
       (friend/authenticate
        {:workflows [workflow] :auth-url "/login"
-        :credential-fn #(assoc % :roles [::user])})
+        :credential-fn credential-fn})
       (wrap-defaults site-defaults)
       wrap-with-logger
       wrap-gzip))
